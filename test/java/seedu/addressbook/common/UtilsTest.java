123dca36b3775c4286a552254379c00d98222bb8
Cheryl Toh <e0035693@u.nus.edu>
2018-02-21 09:47:51 +0800

    Add JUnit tests for isAnyAnyNull

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -43,4 +43,25 @@ public class UtilsTest {
     private void assertNotUnique(Object... objects) {
         assertFalse(Utils.elementsAreUnique(Arrays.asList(objects)));
     }
+
+    @Test
+    public void isAnyNull() throws Exception {
+
+        //  Given objects consist of null
+        assertHasNull("a", null, "b");
+        assertHasNull(null, null);
+
+        // Given objects do not consist of any null
+        assertHasNoNull("abc", "def", "c");
+        assertHasNoNull("abc", "ab", "ABC");
+    }
+
+    private void assertHasNull(Object... objects) {
+        assertTrue(Utils.isAnyNull(objects));
+    }
+
+    private void assertHasNoNull(Object... objects) {
+        assertFalse(Utils.isAnyNull(objects));
+    }
+
 }
